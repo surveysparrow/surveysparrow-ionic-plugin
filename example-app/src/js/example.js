860e8acd34f02fb26ce07aac12fb83c9f6207374
Gokulkrishna-Raju <gokulkrishna.raju@surveysparrrow.com>
2025-01-23 22:05:29 +0530

    Initial Commit

--- example-app/src/js/example.js
+++
@@ -1,6 +0,0 @@
-import { SurveySparrowIonicPlugin } from 'surveysparrow-ionic-plugin';
-
-window.testEcho = () => {
-    const inputValue = document.getElementById("echoInput").value;
-    SurveySparrowIonicPlugin.echo({ value: inputValue })
-}
